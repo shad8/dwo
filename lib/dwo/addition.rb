5ccf6ae9b4c9a3e8ea785c0e10e4f76a44247f2f
urszula Holodniak <uholodniak@gmail.com>
2014-03-29 12:30:00 +0100

    Add two numbers

--- lib/dwo/addition.rb
+++ lib/dwo/addition.rb
@@ -1,5 +1,7 @@
 module Dwo
   class Addition
-
+    def add(a,b)
+      a + b
+    end
   end
 end
--- spec/dwo/addition_spec.rb
+++ spec/dwo/addition_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe Dwo::Addition do
-
+  it 'Add two number' do
+    expect(subject.add(4, 3)).to eq 7
+  end
 end
